367ff5005391124a0d0ba6cee051970db85ffcaa
franciele <francielefernandes126@gmail.com>
2023-05-13 00:56:16 -0300

    ✨ feature: Finalização do projeto

--- App.js
+++ App.js
@@ -2,16 +2,61 @@ import React , {useState}from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View , Image, TouchableOpacity} from 'react-native';
 
+let timer= null;
+let ss= 0;
+let mm= 0;
+let hh= 0;
+
+
 export default function App() {
 
   const [numero, setNumero] =useState(0);
   const [botao, setBotao] =useState('Iniciar')
+ 
+  const [ultimo, setUltimo] =useState(null)
 
   function iniciar(){
+    if(timer !== null){
+      clearInterval(timer);
+      timer = null;
+      setBotao('Iniciar');
+    }else{
+      
+      timer = setInterval(() => {
+        ss++;
+        if(ss==60){
+          ss=0;
+          mm++;
+        }
+        if(mm==60){
+          mm=0;
+          hh++;
+        }
+
+        let format = (hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm ) + ':' + (ss < 10 ? '0' + ss : ss);
+
+        setNumero(format);
+  
+
+        
+      }, 1000);
+      setBotao('Parar');
+    }
    
   }
 
   function limpar(){
+    if(timer !== null){
+      clearInterval(timer);
+      timer = null;
+    }
+
+    setUltimo(numero);
+    setNumero(0);
+    ss=0;
+    mm=0;
+    hh=0;
+    setBotao('Iniciar');
 
   }
   return (
@@ -31,7 +76,9 @@ export default function App() {
       </View>
 
       <View style={styles.areaUltima}>
-        <Text style={styles.textoCorrida}>Ultima tempo: 00:05:00</Text>
+        <Text style={styles.textoCorrida}>
+          {ultimo ? 'Ultimo tempo: ' + ultimo : ''}
+        </Text>
 
       </View>
 
