b6fbe105981996715317964d0be0379a8c1ac833
franciele <francielefernandes126@gmail.com>
2023-05-13 00:00:27 -0300

    🌈 style: front  criado

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
+import React , {useState}from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Image, TouchableOpacity} from 'react-native';
 
 export default function App() {
+
+  const [numero, setNumero] =useState(0);
+  const [botao, setBotao] =useState('Iniciar')
+
+  function iniciar(){
+   
+  }
+
+  function limpar(){
+
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Image source={require('./src/images/crono.png')}  />
+      <Text style={styles.timer}>{numero}</Text>
+
+      <View style={styles.btnArea}>
+        <TouchableOpacity style={styles.btn} onPress={iniciar}>
+          <Text style={styles.btnText}>{botao}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={limpar}>
+          <Text style={styles.btnText}>Limpar</Text>
+        </TouchableOpacity>
+
+      </View>
+
+      <View style={styles.areaUltima}>
+        <Text style={styles.textoCorrida}>Ultima tempo: 00:05:00</Text>
+
+      </View>
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +44,42 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333D54',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  timer:{
+    marginTop: -170,
+    fontSize: 45,
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+  btnArea:{
+    flexDirection: 'row',
+    marginTop: 110,
+    height: 40,
+  },
+  btn:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    height: 40,
+    margin: 17,
+    borderRadius: 9,
+  },
+  btnText:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#333D54',
+  },
+  areaUltima:{
+    marginTop: 40,
+  },
+  textoCorrida:{
+    fontSize: 25,
+    fontStyle: 'italic',
+    color: '#FFF',
+  }
+  
 });
